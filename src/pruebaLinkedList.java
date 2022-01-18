cb09ae572317b0a10b79fc4b00e1ecb29cab5476
Tomas <210099@fppiramide.com>
2022-01-18 22:10:33 +0100

    Sigo probando linkedList

--- src/pruebaLinkedList.java
+++ src/pruebaLinkedList.java
@@ -14,12 +14,20 @@ public class pruebaLinkedList {
 		palabras.add("Segunda palabra");
 		palabras.add("Tercera palabra");
 		palabras.add("Cuarta palabra");
+		palabras.addFirst("Antes de Primera");
 
 		System.out.println(palabras.isEmpty());
 		
 		System.out.println(palabras);
-		
+		System.out.println(palabras.getLast());
+		System.out.println(palabras.indexOf("Tercera palabra"));
+		System.out.println(palabras.poll());
+		System.out.println(palabras.pop());
+		System.out.println(palabras);
+		System.out.println("=================================");
+		System.out.println(palabras.peek());
 		System.out.println(palabras.contains("la palabra"));
+		System.out.println();
 		
 		
 	}
